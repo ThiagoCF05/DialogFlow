1045ad13c2aebcf7bfbd420d0a55f873845e17b7
ThiagoCF05 <thiago.castro.ferreira@gmail.com>
2020-06-14 20:40:47 -0300

    Saving pedidos

--- main.py
+++ main.py
@@ -5,13 +5,24 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
+pedidos = []
+
 @app.route('/', methods=['POST'])
 def main():
     data = request.get_json(silent=True)
 
+    contexts = data['queryResult']['outputContexts']
+    for context in contexts:
+        if 'pizzapedido-followup' in context['name']:
+            parametros = context['parameters']
+            sabor = parametros['sabor']
+            nome = parametros['nome']['name']
+            pedidos.append({ 'nome': nome, 'sabor': sabor })
     print(data)
+    print(pedidos)
+    data['fulfillmentText'] = 'Confirmado. Seu pedido está sendo preparado.'
 
-    return jsonify({})
+    return jsonify(data)
 
 # run Flask app
 if __name__ == "__main__":
